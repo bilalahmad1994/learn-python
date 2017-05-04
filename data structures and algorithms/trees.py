5a976bc1e72b0490c881afa375c2538738ef74de
bilalahmad <bilalshakeel590@gmail.com>
2017-05-05 02:05:54 +0500

    implement trees

--- data structures and algorithms/trees.py
+++ data structures and algorithms/trees.py
@@ -1,37 +1,37 @@
-def BinaryTree(r):
-    return [r, [], []]
+# def BinaryTree(r):
+#     return [r, [], []]
+#
+# def insertLeft(root,newBranch):
+#     t = root.pop(1)
+#     if len(t) > 1:
+#         root.insert(1,[newBranch,t,[]])
+#     else:
+#         root.insert(1,[newBranch, [], []])
+#     return root
+#
+# def insertRight(root,newBranch):
+#     t = root(2)
+#     if len(t) > 1:
+#         root.insert(2,[newBranch,[],t])
+#     else:
+#         root.insert(2,[newBranch,[],[]])
+#     return root
+#
+# def getRootVal(root):
+#     return root[0]
+#
+# def setRootVal(root,newVal):
+#     root[0] = newVal
+#
+# def getLeftChild(root):
+#     return root[1]
+#
+# def getRightChild(root):
+#     return root[2]
+#
+# r=BinaryTree(12)
+# print(r)
+# print(insertLeft(r,4))
+# print(insertLeft(r,5))
+# print(getLeftChild(r))
 
-def insertLeft(root,newBranch):
-    t = root.pop(1)
-    if len(t) > 1:
-        root.insert(1,[newBranch,t,[]])
-    else:
-        root.insert(1,[newBranch, [], []])
-    return root
-
-def insertRight(root,newBranch):
-    t = root.pop(2)
-    if len(t) > 1:
-        root.insert(2,[newBranch,[],t])
-    else:
-        root.insert(2,[newBranch,[],[]])
-    return root
-
-def getRootVal(root):
-    return root[0]
-
-def setRootVal(root,newVal):
-    root[0] = newVal
-
-def getLeftChild(root):
-    return root[1]
-
-def getRightChild(root):
-    return root[2]
-
-r=BinaryTree(12)
-print(r)
-print(insertLeft(r,4))
-print(insertLeft(r,5))
-print(getLeftChild(r))
-print(r)
